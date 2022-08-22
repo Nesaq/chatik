c7c36dca6c481d15d59724e2ce3e0ab2cf34b0f0
NesaQ Nes <kholiknes@gmail.com>
2022-08-22 17:16:40 +0300

    added i18 for channels

--- frontend/src/components/Channels.jsx
+++ frontend/src/components/Channels.jsx
@@ -66,7 +66,7 @@ const Channels = () => {
   return (
     <Col className="col-4 col-md-2 border-end pt-5 px-0 bg-light">
       <div className="d-flex justify-content-between mb-2 ps-4 pe-2">
-        <span>Каналы</span>
+        <span>{t('channels.channels')}</span>
         <Button type='button' onClick={modalHandlerAdd} variant="link" className="p-0 text-primary btn-group-vertical">
             <PlusSquare />
           <span className="visually-hidden">+</span>
